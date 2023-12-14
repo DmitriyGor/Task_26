ae9cc07c3955a9eb651eba79f9e58f297cea5f73
Dmitriy <79133850099@yandex.ru>
2023-12-15 02:19:07 +0700

    Task 26

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Задача 26: 
+Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+456 -> 3
+78 -> 2
+89126 -> 5
+*/
+Console.Write("Enter the number : "); // Введите число 
+
+bool isNumber = int.TryParse(Console.ReadLine(), out int number);
+// isNumber = true, то строка состоит только из чисел
+// false - присутствуют в строке еще и символы
+int count = 0; // Количество цифр в числе//
+if (isNumber) // isNumber == true
+{
+while (number != 0)
+{
+    number/=10;
+    count++;
+}
+Console.Write ($"number of digits in a number:  {count}");
+}
+else
+{
+   Console.WriteLine("Error! The line contains not only numbers, but also other symbols ");
+   // Ошибка! В строчке присутсвуют не только цифры, но и другие символы
+}
